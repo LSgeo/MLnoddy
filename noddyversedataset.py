9e914fb71e4b7e8205cc564c1d8cf88cd85f2e6d
LSgeo <LukeSmith.geo@gmail.com>
2022-05-18 17:11:00 +0800

    Add function to subsample and grid forward models
    Need to:
    - Handle padding the extent to deal with NaNs (blank area on plot)
    - Confirm if this method of gridding is analogous to Min. Curvature

--- noddyversedataset.py
+++ noddyversedataset.py
@@ -15,7 +15,72 @@ def parse_geology(pth, layer):
 
 def parse_geophysics(pth):
     """Return forward model values from Noddy geophysics .mag.gz and .grv.gz"""
-    return np.loadtxt(pth, skiprows=8)
+    return np.loadtxt(pth, skiprows=8, dtype=np.float32)
+
+
+def subsample(line_spacing, sample_spacing, cell_size, heading, *rasters):
+    """Run a mock-survey on a geophysical raster.
+    Designed for use with Noddy forward models, as part of a Pytorch dataset.
+
+    Params:
+        raster: input Tensor forward model
+        line_spacing: in meters, spacing between parallel lines
+        sampling_rate: in meters, spacing between cells along line
+        cs: ground truth cell size, 20 m for Noddy models
+        heading: "NS" for columns as lines, "EW" for rows as lines
+
+    The Noddyverse dataset is a suite of 1 Million 200x200x200 petrophysical
+    voxels, at a designated size of 20 m per pixel. Forward models in the
+    Noddyverse (https://doi.org/10.5194/essd-14-381-2022) are generated
+    as per below:
+        | Geophysical forward models were calculated using a Fourier domain
+        | formulation using reflective padding to minimise (but not remove)
+        | boundary effects. The forward gravity and magnetic field calculations
+        | assume a flat top surface with a 100 m sensor elevation above this
+        | surface and the Earth's magnetic field with vertical inclination,
+        | zero declination and an intensity of 50000nT.
+
+    Note that every single cell of the forward model has a calulated forward
+    model, i.e. they are 200x200, with no interpolation (for a 20 m cell size)
+
+    We simulate an airborne survey, by selecting rows (flight lines) of pixels
+    at every n m. We can (but not by default) also subsample along rows.
+
+    """
+    ls = int(line_spacing / cell_size)
+    sr = int(sample_spacing / cell_size)
+    if heading.upper() in ["EW", "E", "W"]:
+        ls, sr = sr, ls  # swap convention to emulate survey direction
+
+    x, y = np.meshgrid(np.arange(200), np.arange(200), indexing="xy")
+    x = cell_size * x[::ls, ::sr]
+    y = cell_size * y[::ls, ::sr]
+    zs = [raster[::ls, ::sr] for raster in rasters]
+
+    return x, y, zs
+
+
+def grid(x, y, z, cs=20, name="foward"):
+    """Grid a subsampled noddy forward model.
+
+    params:
+        x,y: x, y coordinates
+        z: geophysical response value
+        cs: cell size for interpolating grid
+
+    See docstring for subsample() for further notes.
+    """
+
+    grd = vd.ScipyGridder("cubic").fit((x, y), z)
+    grid_raster = grd.grid(
+        region=[0, cs * 199, 0, cs * 199],
+        spacing=cs,
+        dims=["x", "y"],
+        data_names=name,
+    )
+
+    return grid_raster.get(name).values.astype(np.float32)
+
 
 class NoddyDataset(Dataset):
     """Create a Dataset to access magnetic, gravity, and surface geology
@@ -65,6 +130,23 @@ class NoddyDataset(Dataset):
         f = self.m_dir / self.m_names[index]
         self.gt_mag = torch.from_numpy(parse_geophysics((f).with_suffix(".mag.gz")))
         self.gt_grv = torch.from_numpy(parse_geophysics((f).with_suffix(".grv.gz")))
+        self.data = {
+            "gt": torch.stack((self.gt_mag, self.gt_grv), dim=0),
+            **self.data,
+        }
+
+        if "survey" in self.kwargs:
+            sp = {"ls": 20, "ss": 20, "cs": 20, "h": "NS", **self.kwargs["survey"]}
+            x, y, (self.hr_mag, self.hr_grv) = subsample(
+                *sp.values(), self.gt_mag, self.gt_grv
+            )
+            self.hr_mag = torch.from_numpy(grid(x, y, self.hr_mag))
+            self.hr_grv = torch.from_numpy(grid(x, y, self.hr_grv))
+            self.data = {
+                "hr": torch.stack((self.hr_mag, self.hr_grv), dim=0),
+                **self.data,
+            }
+
         if self.load_geology:
             # This is mildly expensive - Could pass layer to np.loadtxt skips?
             self.data = {
@@ -94,18 +176,37 @@ class NoddyDataset(Dataset):
         return self.data
 
 
+import matplotlib.pyplot as plt
 noddy_model_dir = Path(r"data/DYKE_FOLD_FAULT")
 dset = NoddyDataset(noddy_model_dir, load_geology=True)
 
-
-import matplotlib.pyplot as plt
+dset = NoddyDataset(
+    noddy_model_dir,
+    load_geology=True,
+    survey={"ls": 400, "ss": 20},
+    # augment={},
+)
 
 for i in range(5):
     sample = dset[i]
-    fig, (inp, out, geo) = plt.subplots(1, 3)
-    plt.suptitle(sample["label"])
-    inp.imshow(sample["gt"][0])
-    out.imshow(sample["gt"][1])
+
+    fig, axes = plt.subplots(3, 3, constrained_layout=True)
+    # [ax.set_axis_off() for ax in axes.ravel()]
+    [ax.set_xlim(0, 200) for ax in axes.ravel()]
+    [ax.set_ylim(0, 200) for ax in axes.ravel()]
+    ([mag, grv, geo], [mgd, ggd, ax1_off], [mdf, gdf, ax2_off]) = axes
+
+    ax1_off.set_axis_off()
+    ax2_off.set_axis_off()
+    plt.suptitle(", ".join(sample["label"]))
+    mag.imshow(sample["gt"][0])
+    grv.imshow(sample["gt"][1])
     geo.imshow(sample["geo"])
+
+    mgd.imshow(sample["hr"][0])
+    ggd.imshow(sample["hr"][1])
+
+    mdf.imshow(sample["gt"][0] - sample["hr"][0])
+    gdf.imshow(sample["gt"][1] - sample["hr"][1])
     plt.show()
 
