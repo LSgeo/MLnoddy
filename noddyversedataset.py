e805c898d60785e933a78aeb534a617033b70f44
LSgeo <LukeSmith.geo@gmail.com>
2022-05-18 16:20:38 +0800

    Add initial support for standard augmentations
    Need to:
     - troubleshoot the correct dimension for rotation with a stacked tensor?
     - confirm the generator style works as imagined

--- noddyversedataset.py
+++ noddyversedataset.py
@@ -35,6 +35,7 @@ class NoddyDataset(Dataset):
         self,
         model_dir,
         load_geology=False,
+        **kwargs,
     ):
         super().__init__()
 
@@ -43,9 +44,21 @@ class NoddyDataset(Dataset):
             set([p.name[:-7] for p in sorted(self.m_dir.iterdir())])
         )  # List of unique model names in model_dir - (named after a timestamp)
         self.load_geology = load_geology
+        self.kwargs = kwargs
         self.len = len(self.m_names)
         self.label = self.m_dir.stem.split("_")  # Event 1,2 are STRAT, TILT
 
+    def _augment(self, *tensors):
+        if self.augs.get("hflip") and torch.rand(1) < 0.5:
+            (TF.hflip(t) for t in tensors)
+        if self.augs.get("vflip") and torch.rand(1) < 0.5:
+            (TF.vflip(t) for t in tensors)
+        if self.augs.get("rotate") and torch.rand(1) < 0.5:
+            (TF.rotate(t, 90) for t in tensors)
+        if self.augs.get("noise"):
+            # add x % random noise to datasets.
+            pass
+            # (add_noise(t, noise) for t in tensors)
 
     def _process(self, index):
         """Convert parsed numpy arrays to tensors and augment"""
@@ -59,10 +72,17 @@ class NoddyDataset(Dataset):
                 **self.data,
             }
 
-        self.data = {
-            "gt": torch.stack((self.gt_mag, self.gt_grv), dim=0),
-            **self.data,
-        }
+        if "augment" in self.kwargs:
+            self.augs = {
+                "hflip": True,
+                "vflip": True,
+                "rotate": False,
+                "noise": False,
+                **self.kwargs["augs"],
+            }
+
+            raise NotImplementedError("Need to troubleshoot Tensor shapes")
+            self._augment([t for t in self.data[""]])
 
     def __len__(self):
         return self.len
